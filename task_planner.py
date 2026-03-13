01dfd42403ce8f05fe7c6503141cf4468f4d3b8f
lalita bohra <lalitabohra2006@gmail.com>
2026-03-13 23:45:47 +0530

    Updated database connection and improved task display

--- task_planner.py
+++ task_planner.py
@@ -3,7 +3,7 @@ import mysql.connector
 db = mysql.connector.connect(
     host="localhost",
     user="root",
-    password="Projects4?!@",
+    password="Your_Password",
     database="task_planner"
 )
 
