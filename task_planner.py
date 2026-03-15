d1ddad6d339c009b1ed3e6d766d4f39675efc5e5
lalita bohra <lalitabohra2006@gmail.com>
2026-03-15 19:41:59 +0530

    Added loop for continuous menu and exit option.

--- task_planner.py
+++ task_planner.py
@@ -3,36 +3,41 @@ import mysql.connector
 db = mysql.connector.connect(
     host="localhost",
     user="root",
-    password="Your_Password",
+    password="Projects4?!@",
     database="task_planner"
 )
 
 cursor = db.cursor()
+while True:
 
-print("SMART DAILY TASK PLANNER")
+    print("SMART DAILY TASK PLANNER")
 
-print("1. Add Task")
-print("2. View Tasks")
+    print("1. Add Task")
+    print("2. View Tasks")
+    print("3. Exit")
 
-choice = int(input("Enter your choice: "))
+    choice = int(input("Enter your choice: "))
 
-if choice == 1:
-    title = input("Enter task title: ")
-    description = input("Enter description: ")
-    priority = int(input("Enter priority (1-High, 2-Medium, 3-Low): "))
-    deadline = input("Enter deadline (YYYY-MM-DD): ")
+    if choice == 1:
+        title = input("Enter task title: ")
+        description = input("Enter description: ")
+        priority = int(input("Enter priority (1-High, 2-Medium, 3-Low): "))
+        deadline = input("Enter deadline (YYYY-MM-DD): ")
 
-    query = "INSERT INTO tasks (title, description, priority, deadline, status) VALUES (%s,%s,%s,%s,%s)"
-    values = (title, description, priority, deadline, "Pending")
+        query = "INSERT INTO tasks (title, description, priority, deadline, status) VALUES (%s,%s,%s,%s,%s)"
+        values = (title, description, priority, deadline, "Pending")
 
-    cursor.execute(query, values)
-    db.commit()
+        cursor.execute(query, values)
+        db.commit()
+        print("Task added successfully!")
 
-    print("Task added successfully!")
+    elif choice == 2:
+        cursor.execute("SELECT * FROM tasks")
+        tasks = cursor.fetchall()
 
-elif choice == 2:
-    cursor.execute("SELECT * FROM tasks")
-    tasks = cursor.fetchall()
+        for task in tasks:
+            print(task)
 
-    for task in tasks:
-        print(task)
+    elif choice == 3:
+        print("Exiting program...")
+        break
